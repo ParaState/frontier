c5e4af265be7efa550a732c00cf6ee7af87497f9
Antonio Yang <yanganto@gmail.com>
2021-05-24 20:08:09 +0800

    Fix llvm to version 10

--- shell.nix
+++ shell.nix
@@ -7,23 +7,37 @@ let
   nixpkgs = import <nixpkgs> { overlays = [ mozillaOverlay ]; };
   rust-nightly = with nixpkgs; ((rustChannelOf { date = "2020-10-23"; channel = "nightly"; }).rust.override {
     targets = [ "wasm32-unknown-unknown" ];
-    extensions = [ "rust-src" ];
   });
+  checkScript = nixpkgs.writeShellScriptBin "check-env" ''
+    echo "export from shell"
+    echo LIBCLANG_PATH=$LIBCLANG_PATH
+    echo LLVM_LIBRARY_DIR=$LLVM_LIBRARY_DIR
+    echo " "
+    echo "Find libclang.so in store"
+    find /nix/store | grep libclang.so
+    echo "Find LLVMgold.so in store"
+    find /nix/store | grep LLVMgold.so
+  '';
+  clangStdenv = nixpkgs.llvmPackages_10.stdenv;
 in
-with nixpkgs; pkgs.mkShell {
-  buildInputs = [
-    clang
+clangStdenv.mkDerivation {
+  name = "clang-10-nix-shell";
+  buildInputs = with nixpkgs; [
     cmake
     pkg-config
     rust-nightly
 
     # rust-ssvm
+    llvmPackages_10.llvm
+    llvmPackages_10.libclang
+    lld_10
     boost
-  ] ++ stdenv.lib.optionals stdenv.isDarwin [
+    checkScript
+    protobuf
+  ] ++ lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Security
   ];
-
-  LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
-  PROTOC = "${protobuf}/bin/protoc";
-  ROCKSDB_LIB_DIR = "${rocksdb}/lib";
+  LIBCLANG_PATH = "${nixpkgs.llvmPackages_10.libclang.lib}/lib";
+  LLVM_LIBRARY_DIR = "${nixpkgs.llvmPackages_10.llvm.lib}/lib";
+  PROTOC = "${nixpkgs.protobuf}/bin/protoc";
 }
